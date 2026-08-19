565a28bdd2079055b50aedf26be180d20826615d
mongkholkp001-sys <mongkholkp001@gmail.com>
2026-08-19 09:44:51 +0700

    Update /api/debug-python to verify imports

--- server.js
+++ server.js
@@ -264,7 +264,7 @@ const server = http.createServer((req, res) => {
     // Debug Python endpoint
     if (req.method === 'GET' && req.url === '/api/debug-python') {
         const { exec } = require('child_process');
-        exec('tesseract --version', (err, stdout, stderr) => {
+        exec('python -c "import fitz, pytesseract, PIL, io; print(\'ALL IMPORTS OK\')"', (err, stdout, stderr) => {
             res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
             res.end(`STDOUT:\n${stdout}\nSTDERR:\n${stderr}\nERR:\n${err ? err.message : 'none'}`);
         });
